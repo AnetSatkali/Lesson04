6d19d7b578ec22e7626c0e920a47476300f4ad8e
AnetSatkalicc <liuyrt@mail.ru>
2020-12-24 00:16:14 +0600

    Tortinwi

---
+++ app/src/main/java/com/example/lesson_/MenuActivity4.java
@@ -0,0 +1,34 @@
+package com.example.lesson_;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MenuActivity4 extends AppCompatActivity implements View.OnClickListener {
+    Button btn_Back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu1);
+
+        btn_Back = findViewById(R.id.btn_back);
+        btn_Back.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_back) {
+            Intent main = new Intent(this, MainActivity.class);
+            startActivity(main);
+        }
+    }
+}
